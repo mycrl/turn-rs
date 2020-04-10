6d25b21fc736c0ed539d8e1fb07a45e95b9aeb8f
Mr.Panda <xivistudios@gmail.com>
2020-04-10 18:24:54 +0800

    Update socket.rs

--- src/socket.rs
+++ src/socket.rs
@@ -6,13 +6,22 @@ use bytes::BytesMut;
 use bytes::BufMut;
 use bytes::Bytes;
 
+
+/// # Socket
+/// * `socket` tcp socket connectin.
+/// * `input` socket message buffer.
+/// * `ouput` socket send buffer.
 pub struct Socket {
     socket: TcpStream,
     input: BytesMut,
     output: BytesMut,
 }
 
+
 impl Socket {
+    
+    /// ## create tcp socket.
+    ///
     pub fn new (socket: TcpStream) -> Self {
         Self {
             socket,
@@ -21,27 +30,32 @@ impl Socket {
         }
     }
 
+    /// ## write buffer for socket.
+    ///
     pub fn write (&mut self, data: &[u8]) {
         self.output.reserve(data.len());
         self.output.put(data);
     }
 
+    /// ## read buffer for socket.
+    ///
     pub fn read (&mut self, size: usize) -> Poll<(), Error> {
         loop {
             self.input.reserve(size);
-            let result = self.socket
-                .read_buf(&mut self.input);
+            let result = self.socket.read_buf(&mut self.input);
             let bytes_read = try_ready!(result);
             if bytes_read == 0 {
-                return Ok(Async::Ready(()));
+                return Ok(Async::Ready(()))
             }
         }
     }
 
+    /// ## flush buffer for socket.
+    /// loop for poll.
+    ///
     pub fn flush (&mut self) -> Poll<(), Error> {
         while !self.output.is_empty() {
-            let result = self.socket
-                .poll_write(&self.output);
+            let result = self.socket.poll_write(&self.output);
             let bytes_written = try_ready!(result);
             if bytes_written > 0 {
                 self.output.split_to(bytes_written);
@@ -52,19 +66,22 @@ impl Socket {
     }
 }
 
+
 impl Stream for Socket {
     type Item = Bytes;
     type Error = ();
-    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        let closed = self.read(4096)
-            .unwrap()
-            .is_ready();
+    
+    fn poll (&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        let closed = self.read(4096).unwrap().is_ready();
         let result = self.input.take();
 
+        // if buffer is not empty.
+        // of return buffer.
         if !result.is_empty() {
             return Ok(Async::Ready(Some(result.freeze())))
         }
 
+        // if socket is not closed.
         if closed {
             Ok(Async::Ready(None))
         } else {
@@ -76,6 +93,7 @@ impl Stream for Socket {
 impl Future for Socket {
     type Item = ();
     type Error = ();
+    
     fn poll (&mut self) -> Poll<Self::Item, Self::Error> {
         Ok(Async::Ready(()))
     } 
