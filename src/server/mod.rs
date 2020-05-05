a484b434d78e99b5dd5b01b5cd6112dbf2a56b69
Mr.Panda <xivistudios@gmail.com>
2020-05-05 21:11:21 +0800

    _update;

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -6,9 +6,34 @@ use std::error::Error;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
 
+/// TCP 服务器.
+///
+/// 创建一个TCP服务器，绑定到指定端口地址并处理RTMP协议消息.
+///
+/// # Examples
+///
+/// ```no_run
+/// use server::Server;
+/// use std::error::Error;
+///
+/// fn main() -> Result<(), Box<dyn Error>> {
+///     tokio::run(Server::new("0.0.0.0:1935".parse()?)?);
+///     Ok(())
+/// }
+/// ```
 pub struct Server(TcpListener);
 
 impl Server {
+    /// 创建TCP服务器.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use server::Server;
+    ///
+    /// let addr = "0.0.0.0:1935".parse().unwrap();
+    /// Server::new(addr).await.unwrap();
+    /// ```
     pub fn new(addr: SocketAddr) -> Result<Self, Box<dyn Error>> {
         Ok(Self(TcpListener::bind(&addr)?))
     }
--- src/server/socket.rs
+++ src/server/socket.rs
@@ -2,6 +2,7 @@ use crate::rtmp::Rtmp;
 use futures::prelude::*;
 use bytes::{Bytes, BytesMut};
 use tokio::{net::TcpStream, io::AsyncRead, io::AsyncWrite};
+use std::io::Write;
 
 enum State {
     Data(Bytes),
@@ -9,12 +10,35 @@ enum State {
     Close
 }
 
+/// TcpSocket 会话信息.
+///
+/// 处理TCP数据并交给RTMP模块处理.
+/// 并抽象成Future.
 pub struct Socket {
     stream: TcpStream,
     codec: Rtmp
 }
 
 impl Socket {
+    /// 创建 TcpSocket.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::error::Error;
+    /// use tokio::net::TcpListener;
+    /// use socket::Socket;
+    ///
+    /// async fn main() -> Result<(), Box<dyn Error>> {
+    ///     let addr = "0.0.0.0:1935".parse().unwrap();
+    ///     let mut listener = TcpListener::bind(&addr).await?;
+    ///     
+    ///     loop {
+    ///         let (stream, _) = listener.accept().await?;
+    ///         tokio::spawn(Socket::new(stream));
+    ///     }
+    /// }
+    /// ```
     pub fn new(stream: TcpStream) -> Self {
         Self { 
             stream,
@@ -22,31 +46,99 @@ impl Socket {
         }
     }
 
+    /// 发送数据到 TcpSocket.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::error::Error;
+    /// use futures::future::poll_fn;
+    /// use tokio::net::TcpListener;
+    /// use socket::Socket;
+    ///
+    /// async fn main() -> Result<(), Box<dyn Error>> {
+    ///     let addr = "0.0.0.0:1935".parse().unwrap();
+    ///     let mut listener = TcpListener::bind(&addr).await?;
+    ///     
+    ///     loop {
+    ///         let (stream, _) = listener.accept().await?;
+    ///         let socket = Socket::new(stream);
+    ///
+    ///         poll_fn(|cx| {
+    ///             socket.send(cx, &[0, 1, 2]);
+    ///         });
+    ///     }
+    /// }
+    /// ```
     #[rustfmt::skip]
     pub fn send(&mut self, data: &[u8]) {
-        println!("data send size {:?}", &data.len());
-        let mut offset: usize = 0;
         loop {
-            match self.stream.poll_write(&data[offset..]) {
-                Ok(Async::Ready(size)) => match &data.len() < &offset {
-                    true => { offset += size; },
-                    false => { break; }
-                }, _ => (),
+            match self.stream.write_all(data) {
+                Ok(_) => { break; },
+                _ => (),
             }
         }
     }
 
+    /// 从 TcpSocket 读取数据.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::error::Error;
+    /// use futures::future::poll_fn;
+    /// use tokio::net::TcpListener;
+    /// use socket::Socket;
+    ///
+    /// async fn main() -> Result<(), Box<dyn Error>> {
+    ///     let addr = "0.0.0.0:1935".parse().unwrap();
+    ///     let mut listener = TcpListener::bind(&addr).await?;
+    ///     
+    ///     loop {
+    ///         let (stream, _) = listener.accept().await?;
+    ///         let socket = Socket::new(stream);
+    ///
+    ///         poll_fn(|cx| {
+    ///             socket.read(cx, 128);
+    ///         });
+    ///     }
+    /// }
+    /// ```
     #[rustfmt::skip]
     fn read(&mut self) -> State {
-        println!("read data");
         let mut receiver = [0; 4096];
         match self.stream.poll_read(&mut receiver) {
             Ok(Async::Ready(size)) if size > 0 => State::Data(BytesMut::from(&receiver[0..size]).freeze()),
             Ok(Async::Ready(size)) if size == 0 => State::Close,
-            _ => State::NotData
+            _ => State::NotData,
         }
     }
 
+    /// 刷新 TcpSocket 缓冲区.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::error::Error;
+    /// use futures::future::poll_fn;
+    /// use tokio::net::TcpListener;
+    /// use socket::Socket;
+    ///
+    /// async fn main() -> Result<(), Box<dyn Error>> {
+    ///     let addr = "0.0.0.0:1935".parse().unwrap();
+    ///     let mut listener = TcpListener::bind(&addr).await?;
+    ///     
+    ///     loop {
+    ///         let (stream, _) = listener.accept().await?;
+    ///         let socket = Socket::new(stream);
+    ///
+    ///         poll_fn(|cx| {
+    ///             socket.read(cx, 128);
+    ///             socket.flush(cx);
+    ///         });
+    ///     }
+    /// }
+    /// ```
     pub fn flush(&mut self) {
         loop {
             match self.stream.poll_flush() {
