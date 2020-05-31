30db2c1571119ee3950343a3c0be854e6be74d44
huangjj27 <huangjj.27@qq.com>
2020-05-31 17:07:20 +0800

    优化函数实现

--- exchange/src/server/mod.rs
+++ exchange/src/server/mod.rs
@@ -29,17 +29,6 @@ impl Server {
             listener: TcpListener::bind(addr).await?,
         })
     }
-
-    pub async fn handle(mut self) {
-        while let Ok((socket, addr)) = self.listener.accept().await {
-            let addr_str = Arc::new(addr.to_string());
-            let sender = self.sender.clone();
-            tokio::spawn(Socket::new(socket, addr_str, sender));
-        }
-
-        // 是否需要提前关闭 sender？
-        // drop(self.sender);
-    }
 }
 
 /// 快速运行服务
@@ -47,9 +36,16 @@ impl Server {
 /// 提供简单方便的服务器启动入口.
 pub async fn run(addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
     let (sender, receiver) = tokio::sync::mpsc::unbounded_channel();
-    let server = Server::new(addr, sender).await?;
-    tokio::spawn(server.handle());
+    let mut server = Server::new(addr, sender).await?;
+
     tokio::spawn(Router::new(receiver));
 
+    while let Ok((socket, addr)) = server.listener.accept().await {
+        let addr_str = Arc::new(addr.to_string());
+        let sender = server.sender.clone();
+        tokio::spawn(Socket::new(socket, addr_str, sender));
+    }
+
+    drop(server.sender);
     Ok(())
 }
