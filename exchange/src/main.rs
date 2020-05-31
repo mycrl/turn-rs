3641cc0f91b2d063a38cce08bda32c315ffefb0f
huangjj27 <huangjj.27@qq.com>
2020-05-31 22:32:35 +0800

    Refactor Server take 2

--- exchange/src/main.rs
+++ exchange/src/main.rs
@@ -2,6 +2,6 @@ mod router;
 mod server;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    Ok(server::run("127.0.0.1:1936".parse().unwrap()).await?)
+async fn main() -> Result<(), std::io::Error> {
+    server::run("127.0.0.1:1936".parse().unwrap()).await
 }
--- exchange/src/server/mod.rs
+++ exchange/src/server/mod.rs
@@ -1,51 +1,28 @@
 mod socket;
 
-use crate::router::{Router, Tx};
+use crate::router::Router;
 
 use socket::Socket;
+use std::io;
 use std::net::SocketAddr;
 use std::sync::Arc;
-
-use std::io::Error;
 use tokio::net::TcpListener;
-
-/// TCP服务器
-///
-/// 处理所有连接到交换中心
-/// 的TcpSocket.
-pub struct Server {
-    listener: TcpListener,
-    sender: Tx,
-}
-
-impl Server {
-    /// 创建Tcp服务器实例
-    ///
-    /// 接受写入管道，用于和核心
-    /// 路由之间通信.
-    pub async fn new(addr: SocketAddr, sender: Tx) -> Result<Self, Error> {
-        Ok(Self {
-            sender,
-            listener: TcpListener::bind(addr).await?,
-        })
-    }
-}
 
 /// 快速运行服务
 ///
 /// 提供简单方便的服务器启动入口.
-pub async fn run(addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn run(addr: SocketAddr) -> Result<(), io::Error> {
     let (sender, receiver) = tokio::sync::mpsc::unbounded_channel();
-    let mut server = Server::new(addr, sender).await?;
+    let mut listener = TcpListener::bind(addr).await?;
 
-    tokio::spawn(Router::new(receiver));
+    let router = tokio::spawn(Router::new(receiver));
 
-    while let Ok((socket, addr)) = server.listener.accept().await {
+    while let Ok((socket, addr)) = listener.accept().await {
         let addr_str = Arc::new(addr.to_string());
-        let sender = server.sender.clone();
-        tokio::spawn(Socket::new(socket, addr_str, sender));
+        tokio::spawn(Socket::new(socket, addr_str, sender.clone()));
     }
 
-    drop(server.sender);
-    Ok(())
+    // 这里还有一份已经不需要的 Sender，要先于 Receiver drop掉才不会阻塞 Receiver 关闭
+    drop(sender);
+    router.await?
 }
