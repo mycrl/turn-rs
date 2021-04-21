6375f77d3a85cef8ca54754e45954ce5823ef613
Mr.Panda <xivistudios@gmail.com>
2021-04-22 00:05:10 +0800

    update;

--- lib/random_port/src/lib.rs
+++ lib/random_port/src/lib.rs
@@ -1,5 +1,22 @@
-mod bitmap;
+#![feature(asm)]
 
-pub struct Ports {
-    buckets: [u64; 256]
+pub struct Buckets {
+    inner: [u64; 256]
+}
+
+impl Buckets {
+    pub fn new() -> Self {
+        Self { inner: [0u64; 256] }
+    }
+    
+    fn find(&self, index: usize) -> Option<usize> {
+        let value = self.inner[index];
+        if value == u64::MAX {
+            return None
+        }
+        
+        let index: u64;
+        unsafe { asm!("bsr {}, {}", out(reg) index, in(reg) value) }
+        (63 - index) as usize
+    }
 }
