98925400d3a6ac245eb016149c4c270b04ab161c
Mr.Panda <xivistudios@gmail.com>
2021-06-10 00:28:40 +0800

    feat: update turn and sdp; (#46)
    * update;
    
    * update;
    
    * update;
    
    * update;
    
    * update random port lib;
    
    * update;
    
    * bug fix: random port allocate;
    
    * update;
    
    * update;
    
    * update;
    
    * update;
    
    * update;
    
    * update;
    
    * update;
    
    * up
    
    * feat: unwarp the project lib;
    
    * feat: change the cli library to clap;
    
    * style: rename config model;
    
    * up
    
    * up
    
    * Update Dockerfile
    
    * rename；
    
    * up;
    
    * up;
    
    * feat: add sdp repeat times;
    
    * feat: remove config file support;
    
    * bug fix: update docker cli argv env;

--- bin/turn/entrypoint.sh
+++ bin/turn/entrypoint.sh
@@ -3,9 +3,9 @@
 COMMAND="turn --listen 0.0.0.0:3478"
 
 if [ $TURN_REALM ]; then COMMAND="${COMMAND} --realm ${TURN_REALM}"; fi
-if [ $TURN_LOCAL ]; then COMMAND="${COMMAND} --local ${TURN_LOCAL}"; fi
 if [ $TURN_NATS ]; then COMMAND="${COMMAND} --nats ${TURN_NATS}"; fi
 if [ $TURN_BUFFER ]; then COMMAND="${COMMAND} --buffer ${TURN_BUFFER}"; fi
 if [ $TURN_THREADS ]; then COMMAND="${COMMAND} --threads ${TURN_THREADS}"; fi
+if [ $TURN_EXTERNAL ]; then COMMAND="${COMMAND} --external ${TURN_EXTERNAL}"; fi
 
 /bin/bash -c "${COMMAND}"
