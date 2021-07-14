d9d6e724461fb4d9ed45e1b20e97cfc4472993f5
Mr.Panda <xivistudios@gmail.com>
2021-07-14 17:18:29 +0800

    up: workfolw;

--- dtls/src/lib.rs
+++ dtls/src/lib.rs
@@ -1,17 +1,57 @@
+use num_enum::TryFromPrimitive;
+use std::convert::TryFrom;
+use bytes::Buf;
 
+#[repr(u8)]
+#[derive(TryFromPrimitive)]
 pub enum Content {
-    
+    Handshake = 0x16
 }
 
 pub enum Payload {
+    Handshake(Handshake)   
+}
+
+#[repr(u8)]
+#[derive(TryFromPrimitive)]
+pub enum HandshakeKind {
+    ClientHello = 0x01
+}
+
+pub struct Handshake {
+    kind: HandshakeKind,
+    length: u32,
+    sequence: u16,
+    offset: u32,
+    version: u16,
     
-} 
+}
 
 pub struct Dtls {
     content: Content,
     version: u16,
     epoch: u16,
-    sequence_number: u64,
+    sequence: u64,
     length: u16,
-    payload: Vec<Payload>
+    fragment: Vec<Payload>
+}
+
+impl TryFrom<&[u8]> for Dtls {
+    type Error = anyhow::Error;
+    fn try_from(mut buf: &[u8]) -> Result<Self, Self::Error> {
+        let content = Content::try_from(buf.get_u8())?;
+        let version = buf.get_u16();
+        let epoch = buf.get_u16();
+        let sequence = buf.get_uint(6);
+        let length = buf.get_u16();
+
+        Ok(Self {
+            content,
+            version,
+            epoch,
+            length,
+            sequence,
+            fragment: Vec::with_capacity(5)
+        })
+    }
 }
