ee847091c4ce2cc41fd33303276f1da8f931e66d
Mr.Panda <xivistudios@gmail.com>
2021-07-15 22:34:43 +0800

    up: implement session;

--- dtls/src/server/mod.rs
+++ dtls/src/server/mod.rs
@@ -1,13 +1,22 @@
 mod session;
 
-use std::io::*;
-use std::net::UdpSocket;
+use anyhow::Result;
+use session::Session;
 use std::collections::HashMap;
 use std::net::SocketAddr;
-
-use session::Session;
+use std::net::UdpSocket;
+use std::sync::Arc;
 
 pub struct Server {
-    inner: UdpSocket,
-    sessions: HashMap<SocketAddr, Session>
+    inner: Arc<UdpSocket>,
+    sessions: HashMap<SocketAddr, Session<'static>>
+}
+
+impl Server {
+    pub async fn new(addr: SocketAddr) -> Result<Self> {
+        Ok(Self {
+            inner: Arc::new(UdpSocket::bind(addr)?),
+            sessions: HashMap::with_capacity(1024),
+        })
+    }
 }
--- dtls/src/server/session.rs
+++ dtls/src/server/session.rs
@@ -1,4 +1,29 @@
+use std::{io::*, net::SocketAddr};
+use tokio::sync::mpsc::Receiver;
+use std::net::UdpSocket;
+use std::sync::Arc;
 
-pub struct Session {
-    
+pub struct Session<'a> {
+    socket: Arc<UdpSocket>,
+    reader: Receiver<&'a [u8]>,
+    addr: SocketAddr,
+}
+
+impl<'a> Read for Session<'a> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        let read_buf = self.reader.blocking_recv().unwrap();
+        let read_ptr = &mut read_buf as *mut [u8];
+        unsafe { std::ptr::write(buf as *mut [u8], read_ptr); }
+        Ok(read_buf.len())
+    }
+}
+
+impl Write for Session<'_> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        self.socket.send_to(buf, self.addr)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        Ok(())
+    }
 }
