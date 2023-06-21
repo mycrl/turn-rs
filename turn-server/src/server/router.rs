d219483bdfb050d2b827707759452b5ffb954f00
Mr.Panda <xivistudios@gmail.com>
2023-06-21 11:23:00 +0800

    Update router.rs

--- turn-server/src/server/router.rs
+++ turn-server/src/server/router.rs
@@ -16,6 +16,7 @@ use tokio::sync::mpsc::{
     UnboundedSender,
 };
 
+/// Handles packet forwarding between transport protocols.
 pub struct Router {
     senders: RwLock<HashMap<u8, UnboundedSender<(Vec<u8>, SocketAddr)>>>,
     bits: Mutex<&'static mut BitSlice<u8, Lsb0>>,
@@ -28,7 +29,33 @@ impl Router {
             bits: Mutex::new(unsafe { bits![static mut u8, Lsb0; 1; 255] }),
         }
     }
+    
+    async fn alloc_index(&self) -> Option<u8> {
+        let mut bits = self.bits.lock().await;
+        let index = bits.first_one().map(|i| i as u8)?;
+        bits.set(index as usize, false);
+        Some(index)
+    }
 
+    async fn free_index(&self, index: u8) {
+        self.bits.lock().await.set(index as usize, true);
+    }
+
+    /// Get the endpoint reader for the route.
+    ///
+    /// Each transport protocol is layered according to its own endpoint, and
+    /// the data forwarded to this endpoint can be obtained by routing.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// let router = Router::new()
+    /// let (index, receiver) = router.get_receiver().await;
+    ///
+    /// while let Some(res) = receiver.recv().await {
+    ///     // handle res.
+    /// }
+    /// ```
     pub async fn get_receiver(
         self: &Arc<Self>,
     ) -> (u8, UnboundedReceiver<(Vec<u8>, SocketAddr)>) {
@@ -41,6 +68,25 @@ impl Router {
         (index, receiver)
     }
 
+    /// Send data to router.
+    ///
+    /// By specifying the endpoint identifier and destination address, the route
+    /// is forwarded to the corresponding endpoint. However, it should be noted
+    /// that calling this function will not notify whether the endpoint exists.
+    /// If it does not exist, the data will be discarded by default.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// let router = Router::new()
+    /// let (index, receiver) = router.get_receiver().await;
+    /// 
+    /// router.send(index, "127.0.0.1:8080".parse().unwrap(), b"hello").await;
+    ///
+    /// while let Some((data, target)) = receiver.recv().await {
+    ///     println!("{}, {:?}", data.len(), target);
+    /// }
+    /// ```
     pub async fn send(&self, index: u8, addr: &SocketAddr, data: &[u8]) {
         let mut is_destroy = false;
 
@@ -57,21 +103,25 @@ impl Router {
         }
     }
 
+    /// delete endpoint.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// let router = Router::new()
+    /// let (index, receiver) = router.get_receiver().await;
+    /// 
+    /// router.remove(index).await;
+    /// router.send(index, "127.0.0.1:8080".parse().unwrap(), b"hello").await;
+    ///
+    /// while let Some((data, target)) = receiver.recv().await {
+    ///     println!("{}, {:?}", data.len(), target);
+    /// }
+    /// ```
     pub async fn remove(&self, index: u8) {
         if let Some(sender) = self.senders.write().await.remove(&index) {
             self.free_index(index).await;
             drop(sender)
         }
-    }
-
-    async fn alloc_index(&self) -> Option<u8> {
-        let mut bits = self.bits.lock().await;
-        let index = bits.first_one().map(|i| i as u8)?;
-        bits.set(index as usize, false);
-        Some(index)
-    }
-
-    async fn free_index(&self, index: u8) {
-        self.bits.lock().await.set(index as usize, true);
     }
 }
