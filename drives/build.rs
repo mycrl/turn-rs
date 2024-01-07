4dfff3033005066f6346c895ce537e4c082f3b1b
Mr.Panda <xivistudios@gmail.com>
2024-01-07 14:35:24 +0800

    only rust drive;

--- drives/build.rs
+++ drives/build.rs
@@ -1,10 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut prost_build = prost_build::Config::new();
     prost_build.protoc_arg("--experimental_allow_proto3_optional");
-    prost_build.compile_protos(&["../../protos/balance.proto"], &["../../protos"])?;
+    prost_build.compile_protos(&["../protos/balance.proto"], &["../protos"])?;
 
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(&["../../protos/turn.proto"], &["../../protos"])?;
+        .compile(&["../protos/turn.proto"], &["../protos"])?;
     Ok(())
 }
