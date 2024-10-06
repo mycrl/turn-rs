9e608fe0f255314b447589ff7e5d48b3bdf5e346
Lazy Panda <lepidodendraceae@gmail.com>
2024-10-07 04:06:54 +0800

    feat: cli config;

--- turn-server/src/config.rs
+++ turn-server/src/config.rs
@@ -1,6 +1,10 @@
-use std::{collections::HashMap, fs::read_to_string, net::SocketAddr};
+use std::{collections::HashMap, fs::read_to_string, net::SocketAddr, str::FromStr};
 
-use clap::Parser;
+use anyhow::anyhow;
+use clap::{
+    builder::{PossibleValuesParser, TypedValueParser},
+    Parser,
+};
 use serde::{Deserialize, Serialize};
 
 #[repr(C)]
@@ -9,6 +13,18 @@ use serde::{Deserialize, Serialize};
 pub enum Transport {
     TCP = 0,
     UDP = 1,
+}
+
+impl FromStr for Transport {
+    type Err = anyhow::Error;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        Ok(match value {
+            "udp" => Self::UDP,
+            "tcp" => Self::TCP,
+            _ => return Err(anyhow!("unknown transport: {value}")),
+        })
+    }
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
@@ -109,7 +125,7 @@ impl Default for Api {
     }
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone, Copy)]
 #[serde(rename_all = "lowercase")]
 pub enum LogLevel {
     Error,
@@ -117,6 +133,21 @@ pub enum LogLevel {
     Info,
     Debug,
     Trace,
+}
+
+impl FromStr for LogLevel {
+    type Err = String;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        Ok(match value {
+            "trace" => Self::Trace,
+            "debug" => Self::Debug,
+            "info" => Self::Info,
+            "warn" => Self::Warn,
+            "error" => Self::Error,
+            _ => return Err(format!("unknown log level: {value}")),
+        })
+    }
 }
 
 impl Default for LogLevel {
@@ -177,16 +208,82 @@ pub struct Config {
     pub auth: Auth,
 }
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(
     about = env!("CARGO_PKG_DESCRIPTION"),
     version = env!("CARGO_PKG_VERSION"),
     author = env!("CARGO_PKG_AUTHORS"),
 )]
 struct Cli {
-    /// specify the configuration file path.
-    #[arg(long)]
+    /// Specify the configuration file path
+    ///
+    /// Example: --config /etc/turn-rs/config.toml
+    #[arg(long, short)]
     config: Option<String>,
+    /// Static user password
+    ///
+    /// Example: --auth-static-credentials test=test
+    #[arg(long, value_parser = Cli::parse_credential)]
+    auth_static_credentials: Option<Vec<(String, String)>>,
+    /// Static authentication key value (string) that applies only to the TURN
+    /// REST API
+    #[arg(long)]
+    auth_static_auth_secret: Option<String>,
+    /// An enum representing the available verbosity levels of the logger
+    #[arg(
+        long,
+        value_parser = PossibleValuesParser::new(["trace", "debug", "info", "warn", "error"])
+            .map(|s| s.parse::<LogLevel>()),
+    )]
+    log_level: Option<LogLevel>,
+    /// This option specifies the http server binding address used to control
+    /// the turn server
+    #[arg(long)]
+    api_bind: Option<SocketAddr>,
+    /// This option is used to specify the http address of the hooks service
+    ///
+    /// Example: --api-hooks http://localhost:8080/turn
+    #[arg(long)]
+    api_hooks: Option<String>,
+    /// TURN server listen address
+    #[arg(long)]
+    turn_realm: Option<String>,
+    /// TURN server listen interfaces
+    ///
+    /// Example: --turn-interfaces udp@127.0.0.1:3478/127.0.0.1:3478
+    #[arg(long, value_parser = Cli::parse_interface)]
+    turn_interfaces: Option<Vec<Interface>>,
+}
+
+impl Cli {
+    // [username]:[password]
+    fn parse_credential(s: &str) -> Result<(String, String), anyhow::Error> {
+        let err = || anyhow!("invalid credential str: {}", s);
+
+        let mut iter = s.split(':');
+        Ok((
+            iter.next().ok_or_else(err)?.to_string(),
+            iter.next().ok_or_else(err)?.to_string(),
+        ))
+    }
+
+    // [transport]@[bind]/[external]
+    fn parse_interface(s: &str) -> Result<Interface, anyhow::Error> {
+        let err = || anyhow!("invalid interface str: {}", s);
+
+        let mut iter = s.split('@').take(2);
+        let transport = Transport::from_str(iter.next().ok_or_else(err)?)?;
+
+        let mut iter = iter.next().ok_or_else(err)?.split('/');
+        let bind = iter.next().ok_or_else(err)?.parse::<SocketAddr>()?;
+        let external = iter.next().ok_or_else(err)?.parse::<SocketAddr>()?;
+
+        Ok(Interface {
+            transport,
+            external,
+            bind,
+        })
+    }
 }
 
 impl Config {
@@ -194,11 +291,50 @@ impl Config {
     /// specified, the configuration is read from the configuration file,
     /// otherwise the default configuration is used.
     pub fn load() -> anyhow::Result<Self> {
-        Ok(toml::from_str(
-            &Cli::parse()
-                .config
+        let cli = Cli::parse();
+        let mut config = toml::from_str::<Self>(
+            &cli.config
                 .and_then(|path| read_to_string(path).ok())
                 .unwrap_or("".to_string()),
-        )?)
+        )?;
+
+        // Command line arguments have a high priority and override configuration file
+        // options; here they are used to replace the configuration parsed out of the
+        // configuration file.
+        {
+            if let Some(credentials) = cli.auth_static_credentials {
+                for (k, v) in credentials {
+                    config.auth.static_credentials.insert(k, v);
+                }
+            }
+
+            if let Some(secret) = cli.auth_static_auth_secret {
+                config.auth.static_auth_secret.replace(secret);
+            }
+
+            if let Some(level) = cli.log_level {
+                config.log.level = level;
+            }
+
+            if let Some(bind) = cli.api_bind {
+                config.api.bind = bind;
+            }
+
+            if let Some(hooks) = cli.api_hooks {
+                config.api.hooks.replace(hooks);
+            }
+
+            if let Some(realm) = cli.turn_realm {
+                config.turn.realm = realm;
+            }
+
+            if let Some(interfaces) = cli.turn_interfaces {
+                for interface in interfaces {
+                    config.turn.interfaces.push(interface);
+                }
+            }
+        }
+
+        Ok(config)
     }
 }
